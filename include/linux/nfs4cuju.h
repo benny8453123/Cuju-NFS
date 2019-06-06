afdee80a276ad20fe2d038f3f2aadeeb7afb8730
Ben <benny8453123@gmail.com>
2019-06-07 00:55:14 +0800

    add comment & define write cuju command

--- include/linux/nfs4cuju.h
+++ include/linux/nfs4cuju.h
@@ -1,7 +1,13 @@
+/* For Cuju :
+ * Define Cuju command for Cuju nfs/nfsd function
+ * who need to distinguish command type 
+ * should include this file
+ */
 enum nfs_cuju_cmd_ft {
 	NFS_CUJU_CMD_NONE	=	0,
 	NFS_CUJU_CMD_FT		=	1,
 	NFS_CUJU_CMD_EPOCH	=	2,
 	NFS_CUJU_CMD_COMMIT	=	3,
+	NFS_CUJU_CMD_WRITE	=	4,
 };
 
--- include/linux/nfs4cujuinternal.h
+++ include/linux/nfs4cujuinternal.h
@@ -1,3 +1,8 @@
+/* For Cuju :
+ * It's for kvm to use nfs cuju function
+ * this file should include to Cuju/x86/kvm_main.c
+ */
+
 extern int nfs_cuju_cmd_send(unsigned int,int);
 extern int nfs_cuju_cmd_send2(void *,int);
 extern void *global_filp;
